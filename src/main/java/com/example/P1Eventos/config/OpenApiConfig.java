1e4ce1f40e6ee20237fc44f503c11741008b8a4f
Luiz Albuquerque <lzedsal@gmail.com>
2026-07-02 12:39:41 -0300

    Adiciona configuracao OpenAPI com suporte a autenticacao Bearer JWT

---
+++ src/main/java/com/example/P1Eventos/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.example.P1Eventos.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "IFS Eventos API",
+        version = "1.0",
+        description = "API REST para gerenciamento de eventos acadêmicos do IFS"
+    ),
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
